ca068d4bf36e4c711aafc4bd7bf311b55a90c147
Mohammed Taariq Amin Mansurie <mansurie@pdx.edu>
2025-07-13 12:11:21 -0700

    added file to detect a cycle in a linkedlist

---
+++ linkedList/easy/141. LinkedListCycle.java
@@ -0,0 +1,54 @@
+package linkedList.easy;
+
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head ;
+
+    public static Node tail ;
+
+    public static boolean hasCycle() {
+        Node slow = head;
+        Node fast = head;
+        boolean cycle = false;
+        while(fast != null && fast.next!= null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                cycle = true;
+                break;
+            }
+        }
+        return cycle;
+    }
+
+    public static void main(String[] args) {
+        head = new Node(1);
+        Node temp = new Node(2);
+        head.next = temp;
+        head.next.next = new Node(3);
+        head.next.next.next = temp;
+        // 1 -> 2 -> 3 -> 2
+        System.out.println(hasCycle());
+    }
+}
