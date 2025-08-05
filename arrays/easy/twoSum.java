7e9a22ebfb4f21f57821c8ec826ba7d088d59fa2
Mohammed Taariq Amin Mansurie <mansurie@pdx.edu>
2025-08-04 23:55:44 -0700

    updating two  sum java file

--- arrays/easy/twoSum.java
+++ arrays/easy/twoSum.java
@@ -1,13 +1,24 @@
 class Solution {
-    public static int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i<nums.length ; i++){
-            for ( int j = i+1; j<nums.length; j++){
-                if (nums[i]+ nums[j] == target){
+    // public static int[] twoSum(int[] nums, int target) {
+    //     for (int i = 0; i<nums.length ; i++){
+    //         for ( int j = i+1; j<nums.length; j++){
+    //             if (nums[i]+ nums[j] == target){
+    //                 return new int[] {i,j};
+    //             }
+    //         }
+    //     }
+    //     return new int[] {} ;
+    // }
+    //brute force solution
+    public static int[] twoSum(int[] nums, int target){
+        for(int i=0; i<nums.length; i++){
+            for(int j=i+1; j<nums.length; j++){
+                if(nums[i] + nums[j] == target){
                     return new int[] {i,j};
                 }
             }
         }
-        return new int[] {} ;
+        return new int[] {};
     }
 
     public static void main(String args[]){
@@ -15,6 +26,5 @@ class Solution {
         int target = 6;
         int[] result = twoSum(nums, target);
         System.out.println("Indices: " + result[0] + ", " + result[1]); // result [0 & 1] because it only contains i and j as elements that add up to the target, that's why it is just [0 & 1].
-        
     }
 }
